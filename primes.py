b2a309f7cd1f875556471499f0b41fe2be5fc029
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-09-28 14:30:13 +0100

    add functionality for displaying primes

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    count=0
+    while count<number_of_primes:
+        for num in range(2,100000000000000):
+            prime = True
+            for i in range(2,num):
+                if num%i==0:
+                    prime = False
+            if prime:
+                list.append(num)
+                count+=1
+                if count>=number_of_primes:
+                    break
     return list
+
+#print(primes(10))
